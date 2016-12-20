674b116fa2c6c6fc60802d4e2ea89945651812ef
Yuri Kroz <splanger@gmail.com>
2016-12-20 09:49:59 +0200

    Initialize a list of available metrics

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
     // Initialize data
     var drivers = [];
+    var metrics_names = [];
 
-    var drivers_list = $("#drivers-list").find('option')
-    drivers_list.each(function() {
+    var drivers_options = $("#drivers-list").find('option');
+    drivers_options.each(function() {
         drivers.push(
             {
                 id: $(this).val(),
@@ -14,13 +15,25 @@ $( document ).ready(function() {
     });
 
     var current_driver = {
-        id: drivers_list[0].value,
-        name: drivers_list[0].text,
-        license_number: drivers_list[0].attributes['license_number'].value
+        id: drivers_options[0].value,
+        name: drivers_options[0].text,
+        license_number: drivers_options[0].attributes['license_number'].value
     };
 
+    var metrics_options = $("#metrics-list").find('option');
+    metrics_options.each(function() {
+        metrics_names.push($(this).val())
+    });
+    var current_metric = metrics_options[0].value;
 
+
+    console.log('Drivers:');
     console.log(drivers);
+    console.log('Metrics:');
+    console.log(metrics_names);
+    console.log('---------------------------------------------');
     console.log('Current driver:');
     console.log(current_driver);
+    console.log('Current metric:');
+    console.log(current_metric);
 });
