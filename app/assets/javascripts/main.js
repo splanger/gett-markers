90d9a24b862f1336d4ef3b737749541dc8968678
Yuri Kroz <splanger@gmail.com>
2016-12-20 10:15:40 +0200

    Re-render the map on driver/metric selection

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     var drivers = [];
     var metrics_names = [];
 
-    var drivers_options = $("#drivers-list").find('option');
+    var drivers_list = $("#drivers-list");
+    var drivers_options = drivers_list.find('option');
     drivers_options.each(function() {
         drivers.push(
             {
@@ -20,7 +21,8 @@ $( document ).ready(function() {
         license_number: drivers_options[0].attributes['license_number'].value
     };
 
-    var metrics_options = $("#metrics-list").find('option');
+    var metrics_list = $("#metrics-list")
+    var metrics_options = metrics_list.find('option');
     metrics_options.each(function() {
         metrics_names.push($(this).val())
     });
@@ -37,10 +39,25 @@ $( document ).ready(function() {
     console.log(current_metric);
 
     // Initialize the map
-    init_map(current_driver, current_metric);
+    render_map(current_driver, current_metric);
 
-    // Set listeners
-    //$("#drivers-list")
+
+    /** ========== ATTACH LISTENERS ========== */
+    drivers_list.change(function() {
+        var selected_option = $(this).find('option:selected');
+        current_driver = {
+            id: selected_option[0].value,
+            name: selected_option[0].text,
+            license_number: selected_option[0].attributes['license_number'].value
+        };
+        render_map(current_driver, current_metric);
+    });
+
+    metrics_list.change(function() {
+        var current_metric = $(this).find('option:selected').val()
+        render_map(current_driver, current_metric);
+    });
+
 
     /** ========== FUNCTIONS ========== */
 
@@ -49,7 +66,9 @@ $( document ).ready(function() {
      * @param current_driver An object representing currently selected driver
      * @param current_metric Currently selected metric's name
      */
-    function init_map(current_driver, current_metric) {
+    function render_map(current_driver, current_metric) {
+        console.log('Rendering a map of metric ' + current_metric + ' for ' + current_driver['name']);
+
         // Fetch a list of metric points...
         $("#map").load("/drivers/" + current_driver['id'] +
             "/metrics?metric_name=" + current_metric);
