14597835e9fc94e3dba57d6c6ac52604cc2523a6
Yuri Kroz <splanger@gmail.com>
2016-12-20 09:33:20 +0200

    Initialize drivers list

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
-    // Initialize data (a list of drivers,
+    // Initialize data
+    var drivers = [];
+
+    $("#drivers-list").find('option').each(function() {
+        drivers.push(
+            {
+                id: $(this).val(),
+                name: $(this).text(),
+                license_number: $(this).attr('license_number')
+            }
+        )
+    });
+
+    console.log(drivers)
 });
